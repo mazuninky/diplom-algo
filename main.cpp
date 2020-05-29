3ebb87d30bfa4399bcb5ad90bfff5e022da53ce8
Mazunin Konstantin <mazuninky@gmail.com>
2020-05-29 23:40:01 +0300

    Добавил потоконебезопасную версию алгоритма

--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
 #include "tsan_rtl_dd_impoved.h"
 
+int thread_count = 1;
+unsigned long mutex_count = 0;
+
+int create_thread() {
+    auto tid = thread_count++;
+    __dd::PerformCreateThread(tid);
+    return tid;
+}
+
+unsigned long create_mutex() {
+    auto mutex_addr = mutex_count++;
+    __dd::PerformCreateMutex(mutex_addr);
+    return mutex_addr;
+}
+
+void mutex_lock(int tid, unsigned long mutex_addr) {
+    __dd::PerformBeforeLock(tid, mutex_addr);
+    __dd::PerformAfterLock(tid, mutex_addr);
+}
+
+void mutex_wait_lock(int tid, unsigned long mutex_addr) {
+    __dd::PerformBeforeLock(tid, mutex_addr);
+}
+
+void mutex_unlock(int tid, unsigned long mutex_addr) {
+    __dd::PerformUnlock(tid, mutex_addr);
+}
+
+// T1: m1, m2, m3
+// T2: m3, m1
 int main() {
+    auto t1 = create_thread();
+    auto t2 = create_thread();
+
+
+    auto m1 = create_mutex();
+    auto m2 = create_mutex();
+    auto m3 = create_mutex();
+
+    mutex_lock(t1, m1);
+    mutex_lock(t1, m2);
+
+    mutex_lock(t2, m3);
+    mutex_wait_lock(t1, m3);
+
+    mutex_wait_lock(t2, m1); // <== Deadlock
 
     return 0;
 }
--- tsan_rtl_dd_impoved.h
+++ tsan_rtl_dd_impoved.h
@@ -3,13 +3,19 @@
 
 namespace __dd {
 
+    void PerformCreateMutex(unsigned long mutex_addr);
+
+    void PerformCreateThread(int tid);
+
     void PerformBeforeLock(int tid, unsigned long mutex_addr);
 
     void PerformAfterLock(int tid, unsigned long mutex_addr);
 
     void PerformUnlock(int tid, unsigned long mutex_addr);
 
-    void PerformDestroy(unsigned long mutex_addr);
+    void PerformDestroyMutex(unsigned long mutex_addr);
+
+    void PerformDestroyThread(int tid);
 }  // namespace __dd
 
 #endif  // DD_IMPROVED_H
